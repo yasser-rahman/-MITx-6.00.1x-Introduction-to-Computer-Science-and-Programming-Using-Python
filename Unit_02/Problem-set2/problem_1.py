3b0c11d6b3eef45df4b698ec371524899b5e52ea
yasser-rahman <65809120+yasser-rahman@users.noreply.github.com>
2021-02-17 09:47:43 +0200

    Update problem_1.py

--- Unit_02/Problem-set2/problem_1.py
+++ Unit_02/Problem-set2/problem_1.py
@@ -1,1 +1,9 @@
+balance = 42
+annualInterestRate = 0.2
+monthlyPaymentRate = 0.04
 
+
+for i in range(12):
+    unpaid_balance = balance * (1- monthlyPaymentRate)
+    balance = unpaid_balance * (1 + (annualInterestRate/12))
+print('Remaining balance:{:.2f}'.format(balance))
