7944ced81d3451e737d0d8c10bc52aa59150e0aa
yasser-rahman <65809120+yasser-rahman@users.noreply.github.com>
2021-02-22 13:46:02 +0200

    Update p1_.py

--- Problem_Set_03/p1_.py
+++ Problem_Set_03/p1_.py
@@ -1,1 +1,15 @@
-
+def isWordGuessed(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: boolean, True if all the letters of secretWord are in lettersGuessed;
+      False otherwise
+    '''
+    # FILL IN YOUR CODE HERE...
+    for letter in (list(secretWord)):
+        if letter not in lettersGuessed:
+            return False
+        else:
+            continue
+        
+    return True
