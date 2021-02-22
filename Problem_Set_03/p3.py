8fcd477e8c754c1de494cf8d11d8f748cbc43970
yasser-rahman <65809120+yasser-rahman@users.noreply.github.com>
2021-02-22 15:36:50 +0200

    Create p3.py

---
+++ Problem_Set_03/p3.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+def getAvailableLetters(lettersGuessed):
+    '''
+    lettersGuessed: list, what letters have been guessed so far
+    returns: string, comprised of letters that represents what letters have not
+      yet been guessed.
+    '''
+    import string
+    # FILL IN YOUR CODE HERE...
+    lettersGuessedSet = set(lettersGuessed)
+    y = set(string.ascii_lowercase)
+    availabeLetters = sorted(y.difference(lettersGuessedSet))
+    return ''.join(availabeLetters)
