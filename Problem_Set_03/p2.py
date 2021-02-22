1b409b437c3c64423e69fbdd701dce8ee2bef66c
yasser-rahman <65809120+yasser-rahman@users.noreply.github.com>
2021-02-22 15:09:04 +0200

    Create p2.py

---
+++ Problem_Set_03/p2.py
@@ -0,0 +1,20 @@
+def getGuessedWord(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: string, comprised of letters and underscores that represents
+      what letters in secretWord have been guessed so far.
+    '''
+    # FILL IN YOUR CODE HERE...
+    
+    gessedWordList = ['-' for i in range(len(secretWord))]
+    secretWordList = list(secretWord)
+    for letter in lettersGuessed:
+        if letter in secretWordList:
+            for i in range(len(secretWordList)):
+                if letter == secretWordList[i]:
+                    gessedWordList[i] = secretWordList[i]
+                else: 
+                    continue
+    return ''.join(gessedWordList)
+                
