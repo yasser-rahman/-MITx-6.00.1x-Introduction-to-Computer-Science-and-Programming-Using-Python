51406c9d6a54cfd67089c3f2e1ff320603a68d48
yasser-rahman <65809120+yasser-rahman@users.noreply.github.com>
2021-03-10 14:00:18 +0200

    Create Problem 4 - Hand Length.py

---
+++ Problem_Set_04/Problem 4 - Hand Length.py
@@ -0,0 +1,13 @@
+def calculateHandlen(hand):
+    """ 
+    Returns the length (number of letters) in the current hand.
+    
+    hand: dictionary (string int)
+    returns: integer
+    """
+    # TO DO... <-- Remove this comment when you code this function
+
+    sum = 0
+    for value in hand.values():
+        sum += value
+    return sum
